0950773d14d7c4cf003eb6bbfd158ae98dff53ec
Vitalii Pavlenko <v.pavlenko@criteo.com>
2024-03-06 11:22:05 +0100

    fix ig serializer

--- apps/interest_group/serializer.py
+++ apps/interest_group/serializer.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
+from advertiser.serializer import AdvertiserSerializer
+from bidder.serializer import BidderSerializer
 from interest_group.models import InterestGroup
 
 
 class InterestGroupSerializer(serializers.ModelSerializer):
+    bidder = BidderSerializer(read_only=True)
+    advertiser = AdvertiserSerializer(read_only=True)
+
     class Meta:
         model = InterestGroup
         fields = ['id', 'name', 'bidder', 'description', 'data_fee', 'advertiser', 'availability']
